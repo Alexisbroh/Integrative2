c60c202d2e4ce522e7801929736ba60d7117ee90
AlexisBroh <aamedinag62@gmail.com>
2023-06-30 12:46:15 -0600

    Adding Charts

--- server.js
+++ server.js
@@ -8,9 +8,32 @@ const url = "mongodb+srv://AlexisAdrianMG:AlexisAdrianMG@cluster0.ajugo4s.mongod
 async function connect (){
     try{
         await mongoose.connect(url)
-        console.log("You're connect")
+        console.log("You're connected")
     }catch(error){
         console.log("Fella , you can't connect" + error)
+    }
+}
+async function insert(){
+    try{
+        await client.connect();
+        console.log("Connected to the server successfully");
+
+        const db = client.db(dbName);
+        const col = db.collection("analytics"); //Nombre de la coleccion
+
+        let dataVibes = {
+            "specie": "Chihuahua",
+            "name": "Yiyo"
+        }
+
+        const p = await col.insertOne(dataVibes);
+        const myDoc = await col.findOne();
+    }catch (err){
+        console.log(err.stack);
+    }
+
+    finally{
+        await client.close(); 
     }
 }
 connect();
